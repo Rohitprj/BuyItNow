1057858254a1ecc5e02c6957bb0dd6925458672a
Rohit kumar <rohitprj.1282@gmail.com>
2025-08-29 15:05:55 +0530

    go

--- src/modules/home/organisms/AddCarousal.tsx
+++ src/modules/home/organisms/AddCarousal.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Pressable, Image } from 'react-native';
 import React, { FC, useState } from 'react';
 import { screenHeight, screenWidth } from '@utils/Constants';
 import Carousel from 'react-native-reanimated-carousel';
@@ -6,7 +6,7 @@ import FilmSlip from '../molecules/FilmSlip';
 
 const AddCarousal: FC<{ data: any }> = ({ data }) => {
   const [active, setActive] = useState(0);
-  const baseOption = {
+  const baseOptions = {
     vertical: false,
     width: screenWidth,
     height: screenHeight * 0.8,
@@ -14,9 +14,42 @@ const AddCarousal: FC<{ data: any }> = ({ data }) => {
   return (
     <View>
       <FilmSlip />
+      {/* <Carousel
+        {...baseOptions}
+        loop
+        pagingEnabled
+        snapEnabled
+        autoPlay
+        autoPlayInterval={3000}
+        onSnapToItem={(index: any) => setActive(index)}
+        data={data}
+        renderItem={({ item }: any) => (
+          <Pressable style={styles.imageContainer}>
+            <Image source={item?.image_uri} style={styles.img} />
+          </Pressable>
+        )}
+      /> */}
+
+      {active != null && (
+        <View style={styles.dots}>
+          {data?.data?.map((item: any, index: any) => {
+            return <Text key={index}>Hey</Text>;
+          })}
+        </View>
+      )}
     </View>
   );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imageContainer: { width: '100%', height: '100%' },
+  img: { width: '100%', height: '100%', resizeMode: 'cover' },
+  dots: {
+    flexDirection: 'row',
+    width: 100,
+    alignSelf: 'center',
+    marginTop: 10,
+    justifyContent: 'center',
+  },
+});
 
 export default AddCarousal;
