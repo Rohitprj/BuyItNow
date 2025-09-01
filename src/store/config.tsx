0cba9e6b9c40ba3d2545207677d325bbbb50728b
Rohit kumar <rohitprj.1282@gmail.com>
2025-09-01 18:50:31 +0530

    go

--- src/store/config.tsx
+++ src/store/config.tsx
@@ -2,7 +2,7 @@
 // export const BASE_URL = 'http://192.168.0.115:4000';
 
 // AL
-export const BASE_URL = 'http://192.168.0.117:4000';
+// export const BASE_URL = 'http://192.168.0.117:4000';
 
 // Live Server
-// export const BASE_URL = 'https://cart-craft-backend.onrender.com';
+export const BASE_URL = 'https://cart-craft-backend.onrender.com';
